edac98683e16bc7f2863d76f0e54cbcb1bfcc8bb
Ekenzy-101 <ekeneonyekaba@gmail.com>
2020-08-02 06:32:38 +0100

    feat: User Authentication with flask-login
    Restructed this repo into a package. Add User Authentication and
    privacy to the account route

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,71 +1,20 @@
-from datetime import datetime
-from flask import Flask, render_template, redirect, flash, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from sassutils.wsgi import SassMiddleware
-from flaskblog.forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "38a9b7992e3710d8b125d2eee83a2913"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
     'flaskblog': ('static/sass', 'static/css', '/static/css')
 })
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info" 
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    fullname = db.Column(db.String(40), unique=True,
-                         default="Unknown Member", nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    image_file = db.Column(
-        db.String(20), nullable=False, default="default.jpg")
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship("Post", backref="author", lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False,
-                            default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    image_file = db.Column(
-        db.String(20), nullable=False, default="default-post.jpg")
-    category = db.Column(db.String(40), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}', '{self.category}')"
-
-
-@app.route("/")
-def home():
-    return render_template("home.html", title="Home")
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html", title="About")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f"Account Created For {form.username.data}", category="success")
-        return redirect(url_for("login"))
-    return render_template("register.html", title="Register", form=form)
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(f"Successfully Logged In", category="success")
-        return redirect(url_for("home"))
-    return render_template("login.html", title="Login", form=form)
+from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -16,6 +17,15 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), Length(min=6, max=50), EqualTo("password")])
     submit = SubmitField("SIGN UP")
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username already exists. Please choose another one")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email is  taken. Please choose another one")
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[
