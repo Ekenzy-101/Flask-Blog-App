32a6318653367877b174b1dbc71b74262d4303a7
Ekenzy-101 <ekeneonyekaba@gmail.com>
2020-08-11 12:06:45 +0100

    feat: Initial Deployment

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,4 +1,4 @@
-import os
+from os import environ
 from dotenv import load_dotenv, find_dotenv
 
 # Load Environmental Variables
@@ -6,19 +6,21 @@ ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(dotenv_path=ENV_FILE)
 
+
 class Config:
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.getenv("EMAIL_USER") 
-    MAIL_PASSWORD = os.getenv("EMAIL_PASS")
-    SECRET_KEY = os.getenv("SECRET_KEY") or "38a9b7992e3710d8b125d2eee83a2913"
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "sqlite:///site.db"
+    MAIL_USERNAME = environ.get("EMAIL_USER") 
+    MAIL_PASSWORD = environ.get("EMAIL_PASS")
+    SECRET_KEY = environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
-    AWS_REGION = os.getenv("AWS_REGION")
-    AWS_LOCATION = "https://{}.s3.{}.amazonaws.com/".format(AWS_BUCKET_NAME, AWS_REGION)
+    AWS_ACCESS_KEY_ID = environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_BUCKET_NAME = environ.get("AWS_BUCKET_NAME")
+    AWS_REGION = environ.get("AWS_REGION")
+    AWS_LOCATION = "https://{}.s3.{}.amazonaws.com/".format(
+        environ.get("AWS_BUCKET_NAME"), environ.get("AWS_REGION"))
