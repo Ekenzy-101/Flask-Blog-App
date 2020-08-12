cc72f808d13819b20eab8f2d3541d38126fe9021
Onyekaba Ekenedilichukwu <ekeneonyekaba@gmail.com>
2020-08-12 08:03:54 +0100

    Update commands.py

--- flaskblog/commands.py
+++ flaskblog/commands.py
@@ -1,9 +1,19 @@
 import click
-from flask.cli import with_appcontext
 
 from flaskblog import db
-
-@click.command(name="create_tables")
-@with_appcontext
-def create_tables():
+def create_db():
+    """Creates database"""
     db.create_all()
+    
+def drop_db():
+    """Cleans database"""
+    db.drop_all()
+
+def create_model_table():
+    """ Create table model in the database """
+    Model.__table__.create(db.engine)
+
+def init_app(app):
+    # add multiple commands in a bulk
+    for command in [create_db, drop_db, create_model_table]:
+        app.cli.add_command(app.cli.command()(command))
