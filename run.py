d06399d301056bd5ecfe21779adba4ee7acbf882
Onyekaba Ekenedilichukwu <ekeneonyekaba@gmail.com>
2020-08-12 07:49:44 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from flaskblog import create_app
+from flaskblog.commands import create_tables
 
 app = create_app()
+app.cli.add_command(create_tables)
 
 if __name__ == "__main__":
     app.run(debug=True)
